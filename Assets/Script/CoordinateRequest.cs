556a51eec22067a6e5d7cd7d1461fe5c29666411
PatrickkLiu <liupinyao@live.com>
2020-08-24 16:28:02 +0800

    WebRequestWIP

---
+++ Assets/Script/CoordinateRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Net.Http;
+
+public class CoordinateRequest : MonoBehaviour
+{
+    void Start()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("https://www.example.com"));
+    }
+
+    IEnumerator GetRequest(string yoloURL)
+    {
+        
+        using (UnityWebRequest coordinateRequest = UnityWebRequest.Get(yoloURL))  
+        {
+            // Request and wait for the desired page.
+            yield return coordinateRequest.SendWebRequest();                  
+
+            if (coordinateRequest.isNetworkError)
+            {
+                Debug.LogError(coordinateRequest.error);
+                yield break;
+            }
+
+            //JSONNode coordinateInfo = JSON.Parse(coordinateRequest.downloadHandler.text);
+
+        }
+    }
+}
