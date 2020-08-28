17cc20f9abecd23bd41da6580fe6416091fbe615
unknown <jj@hgeek.net>
2020-08-28 12:24:45 +0800

    listener

--- Assets/Script/CoordinateRequest.cs
+++ Assets/Script/CoordinateRequest.cs
@@ -1,19 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using SimpleJSON;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using System.Net.Http;
-using SimpleJSON;
 
 public class CoordinateRequest : MonoBehaviour
 {
-    void Start()
+    public Transform TestCube;
+
+    void Update()
     {
-        // A correct website page.
-        StartCoroutine(GetRequest("127.0.0.1:5000"));
+        //Link to our YOLO json website
+        StartCoroutine(GetRequest("http://127.0.0.1:5000/", "coordinate"));
     }
 
-    IEnumerator GetRequest(string yoloURL)
+
+
+    IEnumerator GetRequest(string yoloURL,string toyName)
     {
         
         using (UnityWebRequest coordinateRequest = UnityWebRequest.Get(yoloURL))  
@@ -21,18 +23,36 @@ public class CoordinateRequest : MonoBehaviour
             // Request and wait for the desired page.
             yield return coordinateRequest.SendWebRequest();                  
 
+            // Return error if there's any
             if (coordinateRequest.isNetworkError || coordinateRequest.isHttpError)
             {
                 Debug.LogError(coordinateRequest.error);
                 yield break;
             }
 
-            JSONNode coordinateInfo = JSON.Parse(coordinateRequest.downloadHandler.text);
-            string coordinate = coordinateInfo["coordinate"];
-            Debug.Log(coordinate);
+            // Print all coordinates
+            //Debug.Log(coordinateRequest.downloadHandler.text);
 
+
+            // Try to parse
+            JSONNode coordinateInfo = JSON.Parse(coordinateRequest.downloadHandler.text);
+            JSONNode CoordX = coordinateInfo[0][toyName]["x"];
+            JSONNode CoordZ = coordinateInfo[0][toyName]["y"];
+            //print(CoordX);
+            TestCube.position = new Vector3 (CoordZ / 4 + 25F, -1.5f, CoordX / 6);
+
+            /*JSONNode CoordX = coordinateInfo[toyName]["x"];
+             *JSONNode CoordZ = coordinateInfo[toyName]["y"];
+             * 
+             * 
+             * 
+             */
         }
+
+
     }
+
+
 
 
 }
